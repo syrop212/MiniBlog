800e9c76852a7620dc1d7c90be764a339846e0d2
syro <albertocasas05@gmail.com>
2016-03-23 12:21:21 -0700

    basic video 6 again done

--- Blog/public_html/js/blog.js
+++ Blog/public_html/js/blog.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(function () {
+   var APPLICATION_ID = "8BEC3A3D-D036-E24B-FF03-BAA2824E0B00",
+       SECRET_KEY = "A54A9ADE-F30B-9D51-FFD5-DE703B8F1300",
+       VERSION = "v1";
+       
+   Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
+   
+   var user = new Backendless.user();
+   user.
+   
+});

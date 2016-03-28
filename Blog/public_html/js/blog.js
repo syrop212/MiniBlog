c35564af3a5244c09eb333805ff88eb34330b81d
syro <albertocasas05@gmail.com>
2016-03-28 13:26:12 -0700

    basic video 15 done

--- Blog/public_html/js/blog.js
+++ Blog/public_html/js/blog.js
@@ -5,9 +5,20 @@ $(function () {
        
    Backendless.initApp(APPLICATION_ID, SECRET_KEY, VERSION);
    
-   var dataStore = Backendless.Persistence.of(Posts);
-   var post = new Posts({title: "My First Blog Post", content: "My first Blog post content", authorEmail:"albertocasas05@gmail.com"});
-   dataStore.save(post);
+   var postsCollection = Backendless.Persistence.of(Posts).find();
+   
+   
+   console.log(postsCollection);
+   
+   var wrapper = {
+       posts: postsCollection.data
+   };
+   
+   var blogScript =$("#blogs-template").html();
+   var blogTemplate = Handlebars.compile(blogScript);
+   var blogHTML = blogTemplate(wrapper);
+   
+   $(".main-container").html(blogHTML);
    
 }); 
 

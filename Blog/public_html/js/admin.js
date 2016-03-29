27fd26659206e7032f6d2a1e862a794135b5a9ad
syro <albertocasas05@gmail.com>
2016-03-29 12:12:26 -0700

    basic video 20 done

--- Blog/public_html/js/admin.js
+++ Blog/public_html/js/admin.js
@@ -10,6 +10,15 @@ $(function () {
    
    $(".main-container").html(loginTemplate);
    
+   $(document).on('submit', 'form-signin', function(event){
+       event.preventDefault();
+       
+       var data = $(this).serializeArray(),
+           email = data[0].value,
+           password = data[1].value;
+           
+        Backendless.UserService.login(email, password, true, new Backendless.Async(userLoggedIn, gotError));
+   });
 }); 
 
 function Posts(args) {
